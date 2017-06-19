da041900b07c2a67f497a7039d9835fe860dfe09
Sowndharya13197 <sowndharyagopal13@gmail.com>
2017-06-19 02:17:01 -0700

    Create Abc.java

---
+++ Abc.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.Scanner;
+public class Abc {
+    public static void main(String args[])
+    {int i;
+   
+    Scanner s=new Scanner(System.in);
+   String num=s.nextLine();
+    int n=num.length();
+     int m[]=new int[n];
+    char p;//[]=new char[n];
+    int num1=Integer.parseInt(num);
+  while(num1>0)
+  {for(i=0;i<n;i++)
+  { m[i]=num1%10;
+  num1=num1/10;
+  }for(i=n-1;i>=0;i--)
+  {
+      int str=97+m[i]-1;
+  p=(char)str;
+  System.out.println(p);
+    }}}}
